c53faaecaa4bbebe330d5ec884ea7ee356caa7cf
Filipe Moreno <y8.filipe@gmail.com>
2023-10-24 18:27:40 -0300

    mudança na url da api

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4000',
-  // baseURL: 'https://backend.toptic.filipemoreno.com.br',
+  // baseURL: 'http://localhost:4000',
+  baseURL: 'https://backend.toptic.filipemoreno.com.br',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
